97285a588e85f02616d16205f4f06e7cec07489d
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2024-04-23 12:24:06 -0300

    added redirect to campaign players page

---
+++ src/main/resources/static/campaign.js
@@ -0,0 +1,32 @@
+console.log("IMPORTED campaign.js")
+
+document.querySelectorAll(".select-campaign").forEach((selectBtn) => {
+    selectBtn.onclick = function () {
+
+        window.location.href = "/campaigns/" + selectBtn.dataset.campaignId
+    }
+})
+
+document.querySelectorAll(".edit-campaign").forEach((editBtn) => {
+    editBtn.onclick = function () {
+        setEditCampaignModal("Editing campaign " + editBtn.dataset.campaignId)
+    }
+})
+
+document.querySelector(".add-campaign-btn").onclick = function () {
+    setEditCampaignModal("Create Campaign")
+}
+
+document.querySelectorAll(".delete-campaign").forEach((rmBtn) => {
+    rmBtn.onclick = function () {
+        console.log("DELETING", rmBtn.dataset.campaignId)
+    }
+})
+
+function setEditCampaignModal(title, prevCampaignData) {
+    const modal = new bootstrap.Modal(document.getElementById('editCampaignModal'))
+
+    console.log(modal)
+
+    document.querySelector("#editCampaignModal > div > div > div.modal-header > h5").textContent = title
+}
